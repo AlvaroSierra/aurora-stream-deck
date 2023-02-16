58cf89d1333f0107e12d910bc874e288fe06a5c3
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2023-02-16 23:31:40 +0000

    Implemented transfer to position

--- rust/src/actions/mod.rs
+++ rust/src/actions/mod.rs
@@ -2,4 +2,4 @@ mod zoom_to_navaid;
 mod tranfer_to_position;
 
 pub use zoom_to_navaid::ZoomToNavaid;
-
+pub use tranfer_to_position::TransferToPosition;
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,14 +2,15 @@ mod actions;
 
 use stream_deck_sdk::action_manager::ActionManager;
 use stream_deck_sdk::init;
-use crate::actions::ZoomToNavaid;
+use crate::actions::{ZoomToNavaid, TransferToPosition};
 
 
 #[tokio::main]
 async fn main() {
 
 	let actions = ActionManager::new().register(vec![
-		Box::new(ZoomToNavaid)
+		Box::new(ZoomToNavaid),
+		Box::new(TransferToPosition)
 	]);
 
 	let init = init(actions, None).await;

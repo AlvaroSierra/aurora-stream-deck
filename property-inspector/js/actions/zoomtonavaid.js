5ef3d369282e4cc8665f059e658b53987474151b
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2023-02-16 21:54:39 +0000

    Simplified zoomtonavaid.js code

--- property-inspector/js/actions/zoomtonavaid.js
+++ property-inspector/js/actions/zoomtonavaid.js
@@ -3,18 +3,14 @@ function zoomtonavaid(inContext) {
 
     Action.call(this, inContext);
 
-    var piSaveSettings = this.saveSettingsCustom;
+    let piSaveSettings = this.saveSettingsCustom;
 
-    let navaid_input = "<div type=\"textarea\" class=\"sdpi-item\" id=\"required_text\">\n" +
+    document.getElementById("placeholder").innerHTML = "<div type=\"textarea\" class=\"sdpi-item\" id=\"required_text\">\n" +
         "    <div class=\"sdpi-item-label\">NAVAID</div>\n" +
         "    <span class=\"sdpi-item-value\">\n" +
-        "        <input type=\"text\"  id=\"navid\" required></input>\n" +
+        "        <input type=\"text\"  id=\"navid\" required/>\n" +
         "    </span>\n" +
         "</div>"
-
-
-
-    document.getElementById("placeholder").innerHTML = navaid_input
     document.getElementById('navid').value = (settings.navaid === undefined ? "" : settings.navaid);
     document.getElementById('navid').addEventListener("input", navaidChanged)
 

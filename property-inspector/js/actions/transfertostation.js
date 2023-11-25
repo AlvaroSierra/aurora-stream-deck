489168f96279572d4d65757946389da62ef72d10
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2023-11-25 15:45:03 +0000

    fix: Transfer to station fixed incorrect input Id

--- property-inspector/js/actions/transfertostation.js
+++ property-inspector/js/actions/transfertostation.js
@@ -8,11 +8,11 @@ function transfertostation(inContext) {
     document.getElementById("placeholder").innerHTML = "<div type=\"textarea\" class=\"sdpi-item\" id=\"required_text\">\n" +
         "    <div class=\"sdpi-item-label\">ATC position</div>\n" +
         "    <span class=\"sdpi-item-value\">\n" +
-        "        <input type=\"text\"  id=\"navid\" required/>\n" +
+        "        <input type=\"text\"  id=\"station\" required/>\n" +
         "    </span>\n" +
         "</div>"
-    document.getElementById('navid').value = (settings.navaid === undefined ? "" : settings.navaid);
-    document.getElementById('navid').addEventListener("input", navaidChanged)
+    document.getElementById('station').value = (settings.navaid === undefined ? "" : settings.navaid);
+    document.getElementById('station').addEventListener("input", navaidChanged)
 
     function navaidChanged(inEvent) {
         settings.navaid = inEvent.target.value;
